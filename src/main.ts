64af4093cc0e06bcd91cb7eef1be853a98bfb7b4
moritzkalwa <kalwa.moritz@gmail.com>
2022-02-11 17:10:01 +0100

    test: pwa

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import App from "./App.vue";
 import router from "./router";
 import VueSmoothScroll from 'vue3-smooth-scroll';
 
+import { registerSW } from "virtual:pwa-register"
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const updateSW = registerSW({
+  onOfflineReady: () => console.log("offline ready"),
+})
+
 const app = createApp(App);
 
 app.use(router);
